91fc474b26173ba40c8d61c37f4663478db7ed77
Joe Mason <128401233+thatchemistryguy@users.noreply.github.com>
2024-10-19 13:44:27 +0100

    Initial Commit

---
+++ plate_templates.js
@@ -0,0 +1,39 @@
+
+//This .js file contains information for multiple 
+//end-user plates. Each section describes the reagents and
+//the designation they should be given, along with the quantity
+//present in both mL and uL plate sizes. 
+let plate_templates = {
+    suzuki_plate: {
+        catalyst1: {
+            rgs: {
+                1: "XPhos Pd G3",
+                2: "SPhos Pd G3",
+                3: "t-BuXPhos Pd G3", 
+                4: "APhos Pd G3", 
+                5: "PCy3 Pd G3", 
+                6: "DTBPF PdCl2",
+                7: "XPhos Pd G3",
+                8: "SPhos Pd G3",
+                9: "t-BuXPhos Pd G3", 
+                10: "APhos Pd G3", 
+                11: "PCy3 Pd G3", 
+                12: "DTBPF PdCl2",
+                13: "XPhos Pd G3",
+                14: "SPhos Pd G3",
+                15: "t-BuXPhos Pd G3", 
+                16: "APhos Pd G3", 
+                17: "PCy3 Pd G3", 
+                18: "DTBPF PdCl2",
+                19: "XPhos Pd G3",
+                20: "SPhos Pd G3",
+                21: "t-BuXPhos Pd G3", 
+                22: "APhos Pd G3", 
+                23: "PCy3 Pd G3", 
+                24: "DTBPF PdCl2",
+            },
+            mL_quant: 1,
+            uL_quant: 0.25
+        }
+    },
+}
